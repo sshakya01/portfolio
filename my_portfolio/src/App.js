1f931b2a3ba760894941b7e70221bdfb373eb80e
Sajil Shakya <shakya.sajil@gmail.com>
2017-10-23 19:38:45 -0400

    added styling

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Nav from './components/Nav.js';
+import Skill from './components/Skill.js';
+import Contact from './components/Contact.js';
+import Projects from './components/Projects.js';
+import Home from './components/Home.js';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import {Route, Redirect, Link, BrowserRouter as Router } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav/>
+        <main>
+          <Route exact path="/project" component={Projects} />
+          <Route exact path="/skills" component={Skill} />
+          <Route exact path="/contact" component={Contact}/>
+          <Route exact path="/" component= {Home} />
+        </main>
+        <Footer/>
       </div>
     );
   }
